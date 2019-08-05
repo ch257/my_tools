6dddbb3646d38eb9668c3637b7d906af0e966313
ch257 <ch257@yandex.ru>
2019-08-05 12:38:17 +0500

    my_tools

--- scripts/modules/php/Template.php
+++ scripts/modules/php/Template.php
@@ -1,4 +1,6 @@
 <?php
+require_once('C:\Churilin\progs\php\vendor\jpgraph-4.2.7\src\jpgraph.php');
+
 class Template {
 	function __construct() {
 		// $this->errors = Errors:new();
@@ -6,6 +8,7 @@ class Template {
 	}
 	
 	function main($args){
+		$mgraph = new MGraph();
 		return true;
 	}
 }
